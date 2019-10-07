fbe41fc0e6da5c96e995fd58f72ac7c963114794
ilona <ilona_bambeshko@tut.by>
2019-10-07 12:20:25 +0300

    Modify

--- hello_app/app/controllers/application_controller.rb
+++ hello_app/app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
 		def hello
-			render text: "hello, world!", content_type: 'text/plain'
-			#render plain: "hello, world!"
+			#render text: "hello, world!", content_type: 'text/plain'
+			render plain: "hello, world!"
 		end
 
 
